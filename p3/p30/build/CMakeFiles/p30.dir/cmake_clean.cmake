87c056ed0ba353cf93b857cba59ed09627a373b6
kevwang <kev.wang2@gmail.com>
2018-12-11 02:28:50 -0500

    Added working CPU solution and empty directories

---
+++ p3/p30/build/CMakeFiles/p30.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p30.dir/main.cc.o"
+  "CMakeFiles/p30.dir/src/complex.cc.o"
+  "CMakeFiles/p30.dir/src/input_image.cc.o"
+  "p30.pdb"
+  "p30"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/p30.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
